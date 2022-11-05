696b2678d247e85806a61ea3c77fb60320c86903
Jasper Holland <jasper@storysoft.io>
2022-11-05 01:26:35 -0500

    Controls changes

--- Office Chair Racing/Assets/Controls.cs
+++ Office Chair Racing/Assets/Controls.cs
@@ -5,25 +5,40 @@ using UnityEngine;
 public class Controls : MonoBehaviour
 {
     private Rigidbody rb;
-    public float speed = 200f;
+    private Camera cam;
+
+
+    public float kickSpeed, scootSpeed, lookSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
         rb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
-        speed = 200f;
+
+        cam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+
+        lookSpeed = 8f;
+        kickSpeed = 8f;
+        scootSpeed = 2f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Look
+        rb.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+
         // Kick
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            rb.AddForce(new Vector3(0, 0, speed));
-            Debug.Log("boop");
+        if (Input.GetMouseButtonDown(0)) {
+            rb.AddRelativeForce(new Vector3(0, 0, kickSpeed), ForceMode.Impulse);
         }
 
+        // Scoot
+        if (Input.GetMouseButtonDown(1))
+        {
+            rb.AddRelativeForce(new Vector3(0, 0, -1 * scootSpeed), ForceMode.Impulse);
+        }
 
     }
 }
