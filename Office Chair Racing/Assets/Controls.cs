57eed313d92d4702ebb6ad32f588d2e6d28a92d5
Jasper Holland <jasper@storysoft.io>
2022-11-05 00:56:08 -0500

    Controls update

--- Office Chair Racing/Assets/Controls.cs
+++ Office Chair Racing/Assets/Controls.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Controls : MonoBehaviour
 {
     private Rigidbody rb;
-    public float speed;
+    public float speed = 200f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
+        speed = 200f;
     }
 
     // Update is called once per frame
@@ -20,7 +22,8 @@ public class Controls : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) {
             rb.AddForce(new Vector3(0, 0, speed));
             Debug.Log("boop");
-        }        
+        }
+
 
     }
 }
