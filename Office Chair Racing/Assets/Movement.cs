3844feac5957f9ea5f8313710a170b317d12d9ab
NewKid25 <115599059+NewKid25@users.noreply.github.com>
2022-11-05 10:17:32 -0500

    Player rotates with mouse

--- Office Chair Racing/Assets/Movement.cs
+++ Office Chair Racing/Assets/Movement.cs
@@ -6,12 +6,12 @@ public class Movement : MonoBehaviour
 {
     public static void Kick(float kickSpeed, Rigidbody rb)
     {
-        rb.AddRelativeForce(new Vector3(0, 0, kickSpeed), ForceMode.Impulse);
+        rb.AddRelativeForce(new Vector3(0, 0, -1* kickSpeed), ForceMode.Impulse);
     }
 
     public static void Scoot(float scootSpeed, Rigidbody rb)
     {
-        rb.AddRelativeForce(new Vector3(0, 0, -1 * scootSpeed), ForceMode.Impulse);
+        rb.AddRelativeForce(new Vector3(0, 0, scootSpeed), ForceMode.Impulse);
     }
 }
 
--- Office Chair Racing/Assets/PlayerControls.cs
+++ Office Chair Racing/Assets/PlayerControls.cs
@@ -7,7 +7,8 @@ public class PlayerControls : MonoBehaviour
     private Rigidbody rb;
     private Camera cam;
 
-
+    private float cameraDif;
+    private Vector3 mousePos;
     public float kickSpeed, scootSpeed, lookSpeed;
 
     // Start is called before the first frame update
@@ -17,6 +18,7 @@ public class PlayerControls : MonoBehaviour
         rb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
 
         cam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        cameraDif = cam.transform.position.y - rb.transform.position.y;
 
         lookSpeed = 8f;
         kickSpeed = 8f;
@@ -27,7 +29,8 @@ public class PlayerControls : MonoBehaviour
     void Update()
     {
         // Look
-        rb.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        
 
         // Kick
         if (Input.GetMouseButtonDown(0)) {
@@ -42,5 +45,26 @@ public class PlayerControls : MonoBehaviour
             Movement.Scoot(scootSpeed, rb);
         }
 
+    }
+
+    private void FixedUpdate()
+    {
+        /*Debug.Log(Input.mousePosition);
+        Debug.Log(mousePos);
+
+        Vector3 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.z, lookDir.x) * Mathf.Rad2Deg;
+        rb.rotation *= Quaternion.Euler(0, angle * lookSpeed, 0);*/
+
+
+      float  mouseX = Input.mousePosition.x;
+
+      float  mouseY = Input.mousePosition.y;
+
+        Vector3 worldpos = cam.ScreenToWorldPoint(new Vector3(mouseX, mouseY, cameraDif));
+
+        Vector3 lookDirection = new Vector3(worldpos.x, rb.transform.position.y, worldpos.z);
+
+        rb.transform.LookAt(lookDirection);
     }
 }
